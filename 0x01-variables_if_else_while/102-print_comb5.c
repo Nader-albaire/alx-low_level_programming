8334799cc31b026b712e94c9cc744f81c5248268
Nader-lbaire <nader.albaire@gmail.com>
2023-09-08 14:51:45 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,37 +11,30 @@
  *
  * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
 
     int num1, num2;
-
-    for (num1 = 0; num1 <= 99; num1++)
+for (num1 = 0; num1 <= 99; num1++)
     {
-        for (num2 = num1 + 1; num2 <= 99; num2++)
-        {
-            /* Print the first two digits */
-            putchar((num1 / 10) + '0');
-            putchar((num1 % 10) + '0');
-
-            /* Print the separator */
-            putchar(' ');
-
-            /* Print the second two digits */
-            putchar((num2 / 10) + '0');
-            putchar((num2 % 10) + '0');
-
-            /* Print the comma and space unless it's the last combination */
-            if (num1 != 98 || num2 != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+/* Print the first two digits */
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+/* Print the separator */
+putchar(' ');
+/* Print the second two digits */
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+/* Print the comma and space unless it's the last combination */
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
