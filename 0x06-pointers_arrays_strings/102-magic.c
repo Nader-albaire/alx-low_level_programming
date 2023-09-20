8c4af92858bd5ce5cd81f6026e60b5b8d86a717c
Nader-albaire <nader.albaire@gmail.com>
2023-09-20 10:01:05 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * main - Entry point of the program
  *
- * Return: A pointer to the changed string.
+ * Description: This function is the entry point of the program. It initializes
+ * variables and demonstrates indirect modification of an integer array element.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-	*(p + 5) = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+    a[2] = 1024;
+    p = &n;
+
+    /*
+     * Write your line of code here...
+     * Remember:
+     * - You are not allowed to use 'a'.
+     * - You are not allowed to modify 'p'.
+     * - Only one statement.
+     * - You are not allowed to code anything else than this line of code.
+     */
+    *(p + 5) = 98;
+
+    /* Print the updated value of a[2] */
+    printf("a[2] = %d\n", a[2]);
+
+    return (0);
 }
